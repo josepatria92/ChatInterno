b436bb4c47b734d562d765c85634f19754a825d5
josepatria92 <104920484+josepatria92@users.noreply.github.com>
2023-05-08 08:29:23 -0700

    structure chat buttons finish

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -3,8 +3,12 @@
 
 <template>
     <div style="width: 100%; max-width: 400px">
+    <div class="row justify-end q-gutter-md q-mb-xs"> 
+        <q-icon name="edit" color="green" /> 
+        <q-icon name="delete" color="red" />
+     </div>
+
         <q-chat-message
-        name="me"
         :text="['hey, how are you?']"
         sent
       />
